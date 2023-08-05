e410e246997eb4c1e74b632821711fe36b577fca
Norman Zhi <normanzyb@gmail.com>
2023-08-05 12:01:08 +0100

    stm32f7: rcc initialisation

--- stm32f7/src/main.rs
+++ stm32f7/src/main.rs
@@ -4,7 +4,9 @@
 use cortex_m::asm;
 use cortex_m_rt::entry;
 use cortex_m_semihosting::hprintln;
-use cortex_m::peripheral::{syst, Peripherals};
+use cortex_m::peripheral::syst;
+
+use stm32f7::stm32f7x2::{interrupt, CorePeripherals, Peripherals, Interrupt, NVIC, RCC};
 
 // pick a panicking behavior
 // use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
@@ -12,19 +14,38 @@ use cortex_m::peripheral::{syst, Peripherals};
 // use panic_itm as _; // logs messages over ITM; requires ITM support
 use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
 
+pub mod rcc;
+
+const SYSTICK_RATE: u32 = 1000;
+const SYSCLK_RATE: u32 = 216_000_000;
+
 #[entry]
 fn main() -> ! {
-    let peripherals = Peripherals::take().unwrap();
-    let mut systick = peripherals.SYST;
-    systick.set_clock_source(syst::SystClkSource::External);
-    systick.set_reload(16_000_000 / 8);
+    let p = Peripherals::take().unwrap();
+    rcc::rcc_init(&p);
+
+    let cp = CorePeripherals::take().unwrap();
+    let mut systick = cp.SYST;
+    systick.set_clock_source(syst::SystClkSource::Core);
+    systick.set_reload(SYSCLK_RATE / SYSTICK_RATE);
     systick.clear_current();
     systick.enable_counter();
 
+    let mut i: u32 = 0;
     loop {
         while !systick.has_wrapped() {}
+        i += 1;
+        if i != 3000 {continue;}
+
         hprintln!("Hello, world!");
-        //panic!("Oops");
-        //asm::bkpt();
+        i = 0;
     }
+
+    //panic!("Oops");
+    //asm::bkpt();
+}
+
+#[interrupt]
+fn EXTI0() {
+    hprintln!(".");
 }
---
+++ stm32f7/src/rcc.rs
@@ -0,0 +1,63 @@
+//use cortex_m::asm;
+use stm32f7::stm32f7x2::Peripherals;
+
+/// Initialise system clock sources
+pub fn rcc_init(p: &Peripherals) {
+    let rcc = &p.RCC;
+    let pwr = &p.PWR;
+    let flash = &p.FLASH;
+
+    // Enable HSE
+    rcc.cr.modify(|_, w| w.hseon().set_bit());
+    while rcc.cr.read().hserdy().is_not_ready() {}
+    // Switch to HSE
+    rcc.cfgr.modify(|_, w| w.sw().hse());
+    while !rcc.cfgr.read().sws().is_hse() {}
+
+    // Disable HSI and PLL
+    rcc.cr.modify(|_, w| w
+        .hsion().clear_bit()
+        .pllon().clear_bit());
+    while rcc.cr.read().pllrdy().is_ready() {}
+
+    // Configure PLL (HSE, PLLM = 12, PLLN = 270, PLLP = 2, PLLQ = 8)
+    rcc.pllcfgr.write(|w| unsafe {w
+        .pllm().bits(12)
+        .plln().bits(270)
+        .pllp().div2()
+        .pllq().bits(8)
+        .pllsrc().hse()});
+
+    // Enable power controller
+    rcc.apb1enr.modify(|_, w| w.pwren().set_bit());
+    // Regulator voltage scale 1
+    pwr.cr1.write(|w| w.vos().scale1());
+    // Enable PLL
+    rcc.cr.modify(|_, w| w.pllon().set_bit());
+    // Enable Over-drive mode
+    pwr.cr1.modify(|_, w| w.oden().set_bit());
+    while pwr.csr1.read().odrdy().bit_is_clear() {}
+    pwr.cr1.modify(|_, w| w.odswen().set_bit());
+    while pwr.csr1.read().odswrdy().bit_is_clear() {}
+    // Set flash latency
+    // ART enable, prefetch enable, 7 wait states
+    flash.acr.write(|w| w
+        .arten().set_bit()
+        .prften().set_bit()
+        .latency().ws7());
+    // Set AHB & APB prescalers
+    // AHB = 1, APB1 = 4, APB2 = 2
+    rcc.cfgr.write(|w| w
+        .hpre().div1()
+        .ppre1().div4()
+        .ppre2().div2());
+
+    // Wait for PLL lock
+    //while (!(RCC->CR & RCC_CR_PLLRDY));
+    while rcc.cr.read().pllrdy().is_not_ready() {}
+    // Switch to PLL
+    rcc.cfgr.write(|w| w.sw().pll());
+    while !rcc.cfgr.read().sws().is_pll() {}
+    // Select peripheral clocks
+    rcc.dckcfgr2.reset();
+}
